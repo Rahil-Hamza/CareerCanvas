51f3ee0cda805c574b9866da749ca5ecb2c03368
Rahil Hamza <rahilhamza786@gmail.com>
2025-03-15 12:39:18 +0530

    Stopped tracking myfile.env

---
+++ DBMS_LAB_Project/Backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Connected to MySQL");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Career Canvas API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
